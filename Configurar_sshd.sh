87d6ca6e4b972a8097a4198e0f9cd3327032e641
sukigsx <sukigsx.mbsistemas@gmail.com>
2026-04-12 10:05:45 +0200

    Update Configurar_sshd.sh

--- Configurar_sshd.sh
+++ Configurar_sshd.sh
@@ -445,6 +445,18 @@ comprobar_estados(){
         return 1
     fi
 
+    #detectar el puerto de ssh
+    # Puerto configurado
+    servicio_ssh_puerto=$(grep -Ei '^\s*Port ' "$ssh_config" | awk '{print $2}' | tail -n1)
+    servicio_ssh_puerto=${puerto:-22}
+
+
+    # Ver si realmente está escuchando
+    #if ss -tlnp | grep -q ":$puerto"; then
+    #    echo "Escuchando en el puerto: $puerto"
+    #else
+    #    echo "No está escuchando en el puerto configurado"
+    #fi
 }
 
 activar_desactivar_password() {
@@ -479,12 +491,12 @@ echo ""
 comprobar_estados
 echo -e "${azul} --- MENU DE OPCIONES ---${borra_colores}"
 echo ""
-echo -e "${azul}  1. ${amarillo}Activar/Desactivar${borra_colores} servidor ssh. Estado = [${verde} $servicio_ssh ${borra_colores}]"
-echo -e "${azul}  2. ${verde}Activar/Desactivar${borra_colores} demonio del servidor ssh."
-echo -e "${azul}  3. ${borra_colores}Cambiar puerto de escucha del ssh."
-echo -e "${azul}  4. ${amarillo}Activar/Desactivar${borra_colores} reenvío (forwarding) del entorno gráfico"
-echo -e "${azul}  5. ${verde}Activar/desactivar${borra_colores} la autenticación por contraseña. Estado = [ $estado_actual ]"
-echo -e "${azul}  6. ${borra_colores}Editar el fichero de configuracion."
+echo -e "${azul}  1. ${borra_colores}Activar/Desactivar servidor ssh. Estado = [${verde} $servicio_ssh ${borra_colores}]"
+echo -e "${azul}  2. ${borra_colores}Activar/Desactivar demonio del servidor ssh."
+echo -e "${azul}  3. ${borra_colores}Cambiar puerto de escucha del ssh. Estado = [${verde} servicio_ssh_puerto ${borra_colores}]"
+echo -e "${azul}  4. ${borra_colores}Activar/Desactivar reenvío (forwarding) del entorno gráfico"
+echo -e "${azul}  5. ${borra_colores}Activar/desactivar la autenticación por contraseña. Estado = [${verde} $estado_actual ${borra_colores}]"
+echo -e "${azul}  6. ${borra_coloresÇEditar el fichero de configuracion."
 echo ""
 echo -e "${azul} 99. ${borra_colores}Salir."
 echo ""
@@ -493,7 +505,7 @@ read -p " Seleciona opcion del menu -> " opcion
 
 case $opcion in
     1)  #Activar/Desactivarvservidor ssh.
-        activar_desactivar_password
+
         ;;
     2)  #Activar/Desactivar demonio del servidor ssh
 
